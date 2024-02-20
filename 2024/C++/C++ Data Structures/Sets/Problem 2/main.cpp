59e33543aab0fb6a8c5439e9ab6b76b7a3f6ce9f
Anato-Eini <jameskennethacabal@gmail.com>
2024-02-21 05:33:06 +0800

    Sets

---
+++ 2024/C++/C++ Data Structures/Sets/Problem 2/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <unordered_map>
+using namespace std;
+int main(){
+    unordered_map<int, int> map;
+    int size;
+    cout << "Enter the size of set A: ";
+    cin >> size;
+    int setA[size];
+    cout << "Enter the elements for set A:\n";
+    for (int i = 0; i < size; ++i) {
+        cin >> setA[i];
+        map[setA[i]]++;
+    }
+    cout << "Enter the size of set B: ";
+    cin >> size;
+    int setB[size];
+    cout << "Enter the elements for set B:\n";
+    for (int i = 0; i < size; ++i) {
+        cin >> setB[i];
+        map[setB[i]]++;
+    }
+    cout << "The intersection of set A and set B is: ";
+    for(int i: setA)
+        if(map[i] > 1) {
+            cout << i << " ";
+            map[i] = 0;
+        }
+    for(int i: setB)
+        if(map[i] > 1) {
+            cout << i << " ";
+            map[i] = 0;
+        }
+    return 0;
+}
